cac905ff36352e6d9aea39d34902582db1b69b4b
Livan Ojito Villanueva <46691447+livanojitov@users.noreply.github.com>
2019-07-15 15:42:42 -0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -26,9 +26,17 @@ const About = () => {
   return (
     <div className="info">
         <p>The Tic-Tac-Toe game written in React.js</p>
-        <p><label>Autor : </label>Livan Ojito Villanueva</p>
-        <p><label>Source: </label><br/><a href="https://github.com/livanojitov/tic-tac-toe">https://github.com/livanojitov/tic-tac-toe</a></p>
-        <p><label>LinkedIn: </label><br/><a href="https://ca.linkedin.com/in/livan-ojito-villanueva">https://ca.linkedin.com/in/livan-ojito-villanueva</a></p>
+        <p>
+          <label>Autor : </label>Livan Ojito Villanueva
+        </p>
+        <p>
+           <label>Source: </label><br/>
+           <a href="https://github.com/livanojitov/tic-tac-toe">https://github.com/livanojitov/tic-tac-toe</a>
+        </p>
+        <p>
+          <label>LinkedIn: </label><br/>
+          <a href="https://ca.linkedin.com/in/livan-ojito-villanueva">https://ca.linkedin.com/in/livan-ojito-villanueva</a>
+        </p>
     </div>
   )
 }
