47aeaaaf6d22c6245c05765bff8152c3da407570
Livan Ojito Villanueva <46691447+livanojitov@users.noreply.github.com>
2019-07-16 13:15:05 -0700

    Create About.js

---
+++ js/About.js
@@ -0,0 +1,18 @@
+const About = () => {
+    return (
+      <div className="info">
+          <p>The Tic-Tac-Toe game written in React.js</p>
+          <p>
+            <label>Author : </label>Livan Ojito Villanueva
+          </p>
+          <p>
+             <label>Source Code : </label><br/>
+             <a href="https://github.com/livanojitov/tic-tac-toe">https://github.com/livanojitov/tic-tac-toe</a>
+          </p>
+          <p>
+            <label>LinkedIn: </label><br/>
+            <a href="https://ca.linkedin.com/in/livan-ojito-villanueva">https://ca.linkedin.com/in/livan-ojito-villanueva</a>
+          </p>
+      </div>
+    )
+  }
