b96bdcec7e9c3814c7a5e28b27b6f153f3114670
Livan Ojito Villanueva <46691447+livanojitov@users.noreply.github.com>
2019-07-17 11:54:57 -0700

    Update Navigation.js

--- js/Navigation.js
+++ js/Navigation.js
@@ -3,8 +3,8 @@ const Navigation = () => {
     <div className="nav">
       <img src="./images/tic-tac-toe.png"/>
       <ul>
+        <li><NavLink exact to="/">Home</NavLink></li>        
         <li><NavLink exact to="/about">About</NavLink></li>           
-        <li><NavLink exact to="/">Home</NavLink></li>
       </ul>
     </div>
   )
