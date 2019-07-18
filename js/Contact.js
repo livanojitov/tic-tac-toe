b4c451f099e307bd8493d20271f4eea450ae7ccb
Livan Ojito Villanueva <46691447+livanojitov@users.noreply.github.com>
2019-07-18 13:50:31 -0700

    Update Contact.js

--- js/Contact.js
+++ js/Contact.js
@@ -2,20 +2,23 @@ const Contact = () => {
     return (
         <div className="contact">
             <p>
-            <span>Author : </span><br/><span className="me">Livan Ojito Villanueva</span>
+               <span>Author : </span><br/><span className="me">Livan Ojito Villanueva</span>
             </p>
             <p>
-                <span>Source Code : </span>
-                <a href="https://github.com/livanojitov/tic-tac-toe">https://github.com/livanojitov/tic-tac-toe</a>
+               <span>Source Code : </span>
+               <a href="https://github.com/livanojitov/tic-tac-toe">https://github.com/livanojitov/tic-tac-toe</a>
             </p>
             <p>
-            <span>Github: </span>
-            <a href="https://github.com/livanojitov"><br/>https://github.com/livanojitov</a>
+               <span>Github: </span>
+               <a href="https://github.com/livanojitov"><br/>https://github.com/livanojitov</a>
             </p>        
             <p>
-            <span>LinkedIn: </span>
-            <a href="https://ca.linkedin.com/in/livan-ojito-villanueva"><br/>https://ca.linkedin.com/in/livan-ojito-villanueva</a>
-            </p>        
+               <span>LinkedIn: </span>
+               <a href="https://ca.linkedin.com/in/livan-ojito-villanueva"><br/>https://ca.linkedin.com/in/livan-ojito-villanueva</a>
+            </p>    
+            <p>
+               <span>Hire me! </span>
+            </p>                  
         </div>
     )
 } 
