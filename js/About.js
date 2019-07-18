4e6ba046aa4c83e457f2911de2f94a103bdd0612
Livan Ojito Villanueva <46691447+livanojitov@users.noreply.github.com>
2019-07-18 13:42:02 -0700

    Update About.js

--- js/About.js
+++ js/About.js
@@ -1,23 +1,8 @@
 const About = () => {
   return (
     <div className="about">
-        <h3>The Tic-Tac-Toe game</h3>
-        <h4>Written with React.js, Javascript, ES6, HTML and CSS</h4>
-        <p>
-          <span>Author : </span><br/><span className="me">Livan Ojito Villanueva</span>
-        </p>
-        <p>
-            <span>Source Code : </span>
-            <a href="https://github.com/livanojitov/tic-tac-toe">https://github.com/livanojitov/tic-tac-toe</a>
-        </p>
-        <p>
-          <span>Github: </span>
-          <a href="https://github.com/livanojitov"><br/>https://github.com/livanojitov</a>
-        </p>        
-        <p>
-          <span>LinkedIn: </span>
-          <a href="https://ca.linkedin.com/in/livan-ojito-villanueva">https://ca.linkedin.com/in/livan-ojito-villanueva</a>
-        </p>        
+        <span>This Tic-Tac-Toe game is based on the two best soccer players of the planet : Messi and Ronaldo.<br/><br/>
+              It's written with React.js, Javascript, ES6, HTML and CSS</span>        
     </div>
   )
-} 
+}
