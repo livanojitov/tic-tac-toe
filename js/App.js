b305c1c3557ab18004314badb9b1d15b8cb8b8a2
Livan Ojito Villanueva <46691447+livanojitov@users.noreply.github.com>
2019-07-18 11:43:19 -0700

    Update App.js

--- js/App.js
+++ js/App.js
@@ -1,13 +1,39 @@
 const Link = ReactRouterDOM.Link, 
       NavLink = ReactRouterDOM.NavLink, 
       Route = ReactRouterDOM.Route;
-      
-const App = props => (
-  <ReactRouterDOM.HashRouter>
-    <Navigation/>
-    <Route path="/" exact component={Board} />
-    <Route path="/about" component={About} />
-  </ReactRouterDOM.HashRouter>
-)
 
-ReactDOM.render(<App />, document.getElementById('app'));
+class App extends React.Component {      
+    state = {
+      newBoard : []
+    }
+
+    updateHistory = (oldBoard) =>{  
+      const newBoard1 = [...this.state.newBoard,oldBoard];
+      this.setState(() => {
+         return {
+           newBoard : newBoard1
+         }
+      });
+    }
+  
+    deleteHistory = (ind) => {
+      const newBoard = this.state.newBoard.filter((board, i) => i !== ind);
+      this.setState(() => {
+        return {
+         newBoard
+        } 
+      });     
+    }
+    
+    render(){
+      return (
+        <ReactRouterDOM.HashRouter>
+          <Navigation/>
+          <Route path="/"        exact render={props => <Board displayHistory={this.updateHistory} {...props} />} />
+          <Route path="/history" exact render={props => <History deleteHistory={this.deleteHistory} boards={this.state.newBoard} {...props} />} />
+          <Route path="/about"   exact component={About}/>
+        </ReactRouterDOM.HashRouter>
+    )}
+}
+
+ReactDOM.render(<App/>, document.getElementById('app'));
