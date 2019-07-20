b0df0244f60ec5491ed26c12bd2aad6d8361c9a5
Livan Ojito Villanueva <46691447+livanojitov@users.noreply.github.com>
2019-07-20 03:22:41 -0700

    Update Contact.js

--- js/Contact.js
+++ js/Contact.js
@@ -5,10 +5,6 @@ const Contact = () => {
                <span>Author : </span><br/><span className="me">Livan Ojito Villanueva</span>
             </p>
             <p>
-               <span>Source Code : </span>
-               <a href="https://github.com/livanojitov/tic-tac-toe">https://github.com/livanojitov/tic-tac-toe</a>
-            </p>
-            <p>
                <span>Github: </span>
                <a href="https://github.com/livanojitov"><br/>https://github.com/livanojitov</a>
             </p>        
