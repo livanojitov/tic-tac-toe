c5b3d941a48c1c9447622174f04027396da21a91
Livan Ojito Villanueva <46691447+livanojitov@users.noreply.github.com>
2019-07-20 03:22:09 -0700

    Update About.js

--- js/About.js
+++ js/About.js
@@ -2,7 +2,11 @@ const About = () => {
   return (
     <div className="about">
         <span>This Tic-Tac-Toe game is dedicated to all the soccer fans.<br/><br/>
-              It's written with React.js, Javascript, ES6, HTML and CSS</span>        
+              It's written with React.js, Javascript, ES6, HTML and CSS</span>
+        <p>
+            <span>Source Code : </span>
+            <a href="https://github.com/livanojitov/tic-tac-toe">https://github.com/livanojitov/tic-tac-toe</a>
+        </p>                     
     </div>
   )
-}
+} 
