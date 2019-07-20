4e8f62678fb8453eb5a8dc215598951010232970
Livan Ojito Villanueva <46691447+livanojitov@users.noreply.github.com>
2019-07-19 20:20:05 -0700

    Update About.js

--- js/About.js
+++ js/About.js
@@ -1,7 +1,7 @@
 const About = () => {
   return (
     <div className="about">
-        <span>This Tic-Tac-Toe game is based on the two best soccer players of the planet : Messi and Ronaldo.<br/><br/>
+        <span>This Tic-Tac-Toe game is dedicated to all the soccer fans.<br/><br/>
               It's written with React.js, Javascript, ES6, HTML and CSS</span>        
     </div>
   )
