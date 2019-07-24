9b557623aadb96259e74063538e14c28ec2397b2
Livan Ojito Villanueva <46691447+livanojitov@users.noreply.github.com>
2019-07-24 09:11:37 -0700

    Add files via upload

--- js/Contact.js
+++ js/Contact.js
@@ -2,18 +2,18 @@ const Contact = () => {
   return (
     <div className="contact">
       <p>
-          <span>Author : </span><br/><span className="me">Livan Ojito Villanueva</span>
+          <span>Author : </span><span className="me">Livan Ojito Villanueva</span>
       </p>
       <p>
-      <span>Email : </span><br/><span className="me">livanojito@gmail.com</span>
+          <span>Email : </span><span className="me">livanojito@gmail.com</span>
       </p>                  
       <p>
           <span>Github: </span>
-          <a href="https://github.com/livanojitov"><br/>https://github.com/livanojitov</a>
+          <a href="https://github.com/livanojitov">https://github.com/livanojitov</a>
       </p>        
       <p>
           <span>LinkedIn: </span>
-          <a href="https://ca.linkedin.com/in/livan-ojito-villanueva"><br/>https://ca.linkedin.com/in/livan-ojito-villanueva</a>
+          <a href="https://ca.linkedin.com/in/livan-ojito-villanueva">https://ca.linkedin.com/in/livan-ojito-villanueva</a>
       </p>    
       <p>
           <span>Hire me! </span>
