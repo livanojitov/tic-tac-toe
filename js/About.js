653b6a32f562d6a27d4ab3e0dc650c91e6455a46
Livan Ojito Villanueva <46691447+livanojitov@users.noreply.github.com>
2019-07-26 17:33:50 -0700

    Add files via upload

--- js/About.js
+++ js/About.js
@@ -1,12 +1,17 @@
 const About = () => {
   return (
     <div className="about">
-        <span>This Tic-Tac-Toe game is dedicated to all the soccer fans.<br/><br/>
-              It's written with React.js, Javascript, ES6, HTML and CSS</span>
         <p>
-            <span>Source Code : </span>
+           <span>This Tic-Tac-Toe game is written with React.js, Javascript, ES6, HTML and CSS</span>
+        </p>
+        <p>
+            <span>Source Code: </span>
             <a href="https://github.com/livanojitov/tic-tac-toe">https://github.com/livanojitov/tic-tac-toe</a>
-        </p>                     
+        </p>
+        <p>
+            <span>Matching pairs game built with Vue.js: </span>
+            <a href="https://livanojitov.github.io/matching-pairs">https://livanojitov.github.io/matching-pairs</a>
+        </p>                 
     </div>
   )
 } 
