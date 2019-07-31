91c8db6e2a94e78cfd5e469aec8337598e85b797
Livan Ojito Villanueva <46691447+livanojitov@users.noreply.github.com>
2019-07-31 10:02:19 -0700

    Add files via upload

--- js/About.js
+++ js/About.js
@@ -1,19 +1,27 @@
 const About = () => {
   return (
     <div className="about">
-        <p>
-           <span>This Tic-Tac-Toe game is written with React.js, Javascript, ES6, HTML and CSS</span>
-        </p>
-        <p>
-            <span>Source Code: </span>
-            <a href="https://github.com/livanojitov/tic-tac-toe">https://github.com/livanojitov/tic-tac-toe</a>
-        </p>
-       
-        <span>Other games: </span>            
-        
+      <p>
+      <span>This game is written with: </span>
         <ul>
-          <li><a href="https://livanojitov.github.io/matching-pairs">https://livanojitov.github.io/matching-pairs ( built with Vue.js )</a></li>
-        </ul>                 
+          <li><a target="_blank" href="https://reactjs.org/">React.js</a></li>
+          <li><a target="_blank" href="https://developer.mozilla.org/en-US/docs/Web/JavaScript">Javascript</a></li>
+          <li><a target="_blank" href="http://es6-features.org">ES6</a></li>
+          <li><a target="_blank" href="https://developer.mozilla.org/en-US/docs/Learn/HTML">Html</a></li>
+          <li><a target="_blank" href="https://developer.mozilla.org/en-US/docs/Web/CSS">Css</a></li>
+        </ul>  
+      </p>        
+      <p>
+          <span>Source Code: </span>
+          <a target="_blank" href="https://github.com/livanojitov/tic-tac-toe">https://github.com/livanojitov/tic-tac-toe</a>
+      </p>
+      
+      <span>Other games: </span>            
+      <ul>
+        <li>
+          <a target="_blank" href="https://livanojitov.github.io/matching-pairs">Matching pairs ( built with Vue.js )</a>
+        </li>
+      </ul>                 
     </div>
   )
 } 
