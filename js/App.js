c64eb325f863b04ba377e11c0ea88942779a1158
Livan Ojito Villanueva <46691447+livanojitov@users.noreply.github.com>
2019-07-31 11:30:59 -0700

    Add files via upload

--- js/App.js
+++ js/App.js
@@ -1,6 +1,7 @@
 const Link = ReactRouterDOM.Link, 
       NavLink = ReactRouterDOM.NavLink, 
-      Route = ReactRouterDOM.Route;
+      Route = ReactRouterDOM.Route,
+      Redirect = ReactRouterDOM.Redirect;
 
 class App extends React.Component {      
   state = {
@@ -16,15 +17,24 @@ class App extends React.Component {
   }
   
   render(){
-    return (
-      <ReactRouterDOM.HashRouter>
-        <Navigation/>
-        <Route path="/"        exact render={props => <TicTacToe updateStore={this.updateStore} {...props} />} />
-        <Route path="/history" exact render={props => <History deleteFromStore={this.deleteFromStore} boards={this.state.boards} {...props} />} />
-        <Route path="/about"   exact component={About}/>
-        <Route path="/contact" exact component={Contact}/>
-      </ReactRouterDOM.HashRouter>
-  )}
+    if (sessionStorage.redirect) {
+      const redirect = sessionStorage.redirect
+      delete sessionStorage.redirect
+      return (
+        <Redirect to={redirect}></Redirect>
+      )
+    }else{
+      return (
+        <ReactRouterDOM.BrowserRouter basename="/tic-tac-toe">
+          <Navigation/>
+          <Route path="/"        exact render={props => <TicTacToe updateStore={this.updateStore} {...props} />} />
+          <Route path="/history" exact render={props => <History deleteFromStore={this.deleteFromStore} boards={this.state.boards} {...props} />} />
+          <Route path="/about"   exact component={About}/>
+          <Route path="/contact" exact component={Contact}/>
+        </ReactRouterDOM.BrowserRouter>
+      )
+    }
+  }
 }
 
 ReactDOM.render(<App/>, document.getElementById('app'));
