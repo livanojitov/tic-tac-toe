3c4d063261dd437e727fb1aa7761b5820507e24f
Livan Ojito Villanueva <46691447+livanojitov@users.noreply.github.com>
2019-07-31 11:15:54 -0700

    Add files via upload

--- js/About.js
+++ js/About.js
@@ -1,8 +1,9 @@
 const About = () => {
   return (
     <div className="about">
-      <p>
-      <span>This game is written with: </span>
+
+      <div>
+        <span>This game is written with: </span>
         <ul>
           <li><a target="_blank" href="https://reactjs.org/">React.js</a></li>
           <li><a target="_blank" href="https://developer.mozilla.org/en-US/docs/Web/JavaScript">Javascript</a></li>
@@ -10,7 +11,8 @@ const About = () => {
           <li><a target="_blank" href="https://developer.mozilla.org/en-US/docs/Learn/HTML">Html</a></li>
           <li><a target="_blank" href="https://developer.mozilla.org/en-US/docs/Web/CSS">Css</a></li>
         </ul>  
-      </p>        
+      </div> 
+
       <p>
           <span>Source Code: </span>
           <a target="_blank" href="https://github.com/livanojitov/tic-tac-toe">https://github.com/livanojitov/tic-tac-toe</a>
@@ -21,7 +23,8 @@ const About = () => {
         <li>
           <a target="_blank" href="https://livanojitov.github.io/matching-pairs">Matching pairs ( built with Vue.js )</a>
         </li>
-      </ul>                 
+      </ul>    
+                   
     </div>
   )
 } 
