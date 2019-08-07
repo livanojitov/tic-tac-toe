ae06dc24af06d2c3d0478f2edc4d7740bfff9e03
Livan Ojito Villanueva <46691447+livanojitov@users.noreply.github.com>
2019-08-07 14:04:05 -0700

    Update App.js

--- js/App.js
+++ js/App.js
@@ -7,7 +7,7 @@ class App extends React.Component {
       return (
         <GameContextProvider>
           <ReactRouterDOM.BrowserRouter basename="/tic-tac-toe">
-            withRouter(Navigation)
+            <Navigation/>
             <Switch>
               <Route path="/"        exact component={Game}        />
               <Route path="/history" exact component={History}     /> 
