31b7da96639c3d22b192010a6ae161b9a59fdebc
Livan Ojito <livanojito@gmail.com>
2019-08-08 23:18:59 -0700

    deleting folders

--- js/Info.js
+++
@@ -1,8 +0,0 @@
-const Info = (props) => {
-  return (
-    <div className="info">  
-        <span>Game over : {props.message}</span>&nbsp;&nbsp;
-        <input type="button" value="Play again" onClick={props.startOver} />
-    </div>
-  )
-}
