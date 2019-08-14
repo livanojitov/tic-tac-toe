e25c3c6c3c2b850fe1471eea45caa04469803140
Livan Ojito <livanojito@gmail.com>
2019-08-14 07:00:51 -0700

    Forgot the basename

--- src/App.js
+++ src/App.js
@@ -8,10 +8,10 @@ import About               from './components/About';
 import Contact             from './components/Contact';
 import GameHistory         from './components/GameHistory';
 
-function App() {// basename="/tic-tac-toe"
+function App() {
   return (
     <GameContextProvider>
-      <BrowserRouter  >
+      <BrowserRouter basename="/tic-tac-toe" >
         <Navigation/>
         <Switch>
           <Route path="/"        exact component={Game}        />
