5916aef427a86a134ad4ece03ac435e1d61927c0
Livan Ojito <livanojito@gmail.com>
2019-08-23 18:51:13 -0700

    Making the path of the images absolute

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Header = () => {
   return (
     <header>
-      <img src={'./images/logo.jpg'} alt="Logo"/>
+      <img src={'/images/logo.jpg'} alt="Logo"/>
     </header>
   )
 }
--- src/components/Square.js
+++ src/components/Square.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Square = ({ win, disabled, handleClick, id, player }) => {
   return (
-    <button style     = {{ backgroundImage : `url(images/${player}.jpg)`}}
+    <button style     = {{ backgroundImage : `url(/images/${player}.jpg)`}}
             className = {win}
             disabled  = {disabled} 
             onClick   = {(e) => { if (handleClick){ handleClick(e)} }}
